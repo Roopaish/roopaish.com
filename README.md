8b58f7334c6540c1537112d5bb801a39dd3ca154
roopaish <rupesh39943@gmail.com>
2024-01-25 22:19:29 +0545

    nextjs project setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Personal website
+
+
+## How to run
+
+- `pnpm install`
+- On linux only: `chmod ug+x .husky/*`
+- Copy .env.example to .env and fill the values
+- `pnpm dev`
---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import withMDX from "@next/mdx";
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  images: {
+    domains: [],
+  },
+};
+
+export default withMDX()(nextConfig);
